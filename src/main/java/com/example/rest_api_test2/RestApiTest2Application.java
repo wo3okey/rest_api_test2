6294179bc93d21c2aa005d8d25c91c9fa35de2fe
we-chall <wo3okey@naver.com>
2021-06-30 23:50:44 +0900

    init

---
+++ src/main/java/com/example/rest_api_test2/RestApiTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest_api_test2/RestApiTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
